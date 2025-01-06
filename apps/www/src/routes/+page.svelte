bc047266dcdd0336dd2dd633d4b97f3d6c858f75
Yann イーベス Eves <hello@yanneves.com>
2025-01-06 13:31:01 +0000

    feat(apps/www): Created website

---
+++ apps/www/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import "@fontsource/inter";
+</script>
+
+<figure class="grid h-screen w-screen place-content-center">
+  <figcaption
+    class="bg-transparent bg-gradient-to-r from-blaze-300 to-blaze-500 bg-clip-text p-9 font-[Inter] font-semibold text-transparent"
+    style="font-size: 25vmin; line-height: 1;"
+  >
+    WWW .
+  </figcaption>
+</figure>
+
+<style>
+  figure {
+    mask-type: luminance;
+    mask: url(data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PScwIDAgNDAwIDQwMCcgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJz4NCiAgPGZpbHRlciBpZD0nbm9pc2VGaWx0ZXInPg0KICAgIDxmZVR1cmJ1bGVuY2UgDQogICAgICB0eXBlPSdmcmFjdGFsTm9pc2UnIA0KICAgICAgYmFzZUZyZXF1ZW5jeT0nMS4xJyANCiAgICAgIG51bU9jdGF2ZXM9JzYnIA0KICAgICAgc3RpdGNoVGlsZXM9J3N0aXRjaCcvPg0KICA8L2ZpbHRlcj4NCiAgDQogIDxyZWN0IHdpZHRoPScxMDAlJyBoZWlnaHQ9JzEwMCUnIGZpbHRlcj0ndXJsKCNub2lzZUZpbHRlciknLz4NCjwvc3ZnPg==);
+  }
+</style>
---
+++ apps/www/svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from "@sveltejs/adapter-auto";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  compilerOptions: {
+    runes: true,
+  },
+
+  kit: {
+    adapter: adapter(),
+  },
+};
+
+export default config;
