66992444c8228bcf2dfc83ec5bff92f61ab8e0aa
Yann イーベス Eves <hello@yanneves.com>
2025-02-05 23:12:49 +0000

    feat(apps/www): Switched to variable fonts elsewhere

--- apps/www/src/routes/+page.svelte
+++ apps/www/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import "@fontsource/inter";
+  import "@fontsource-variable/inter";
   import trackEvent from "$lib/utils/trackEvent";
   import CertificateAuthority from "../lib/components/CertificateAuthority.svelte";
 </script>
