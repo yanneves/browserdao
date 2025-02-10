0ec290aeaa81d72622b3380f7392a8ea92e21732
Yann イーベス Eves <hello@yanneves.com>
2025-02-10 21:17:52 +0000

    feat(apps/chat): Added application shell with sidebar

---
+++ apps/chat/src/lib/components/Shell.svelte
@@ -0,0 +1,93 @@
+<script>
+  import FilmGrain from "$lib/components/FilmGrain.svelte";
+  import { sineInOut } from "svelte/easing";
+
+  let { header, nav, main } = $props();
+  let navDrawerOpen = $state.raw(false);
+
+  function drawer(node, { delay = 0, duration = 150, easing = sineInOut }) {
+    const existingTransform = getComputedStyle(node).transform.replace(
+      "none",
+      "",
+    );
+
+    return {
+      delay,
+      duration,
+      easing,
+      css: (t, u) => `transform: ${existingTransform} translateX(${u * -100}%)`,
+    };
+  }
+</script>
+
+<FilmGrain>
+  <div class="bg-bg-50 h-full"></div>
+</FilmGrain>
+
+{#snippet sidebar()}
+  <div
+    class="bg-bg-50/20 ring-bg-50 flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-4 ring-1 backdrop-blur lg:bg-transparent lg:ring-0 lg:backdrop-blur-none"
+  >
+    <div class="flex h-16 shrink-0 items-center justify-end">
+      {#if navDrawerOpen}
+        <button
+          type="button"
+          class="flex items-center p-2"
+          onclick={() => (navDrawerOpen = false)}
+        >
+          <span class="sr-only">Close sidebar</span>
+          <i class="iconify material-symbols--close-rounded size-6"></i>
+        </button>
+      {/if}
+    </div>
+    <nav class="flex flex-1 flex-col">
+      {@render nav()}
+    </nav>
+  </div>
+{/snippet}
+
+<div class="mx-auto h-full max-w-7xl">
+  {#if navDrawerOpen}
+    <!-- Off-canvas menu for mobile -->
+    <div class="relative z-50 lg:hidden" role="dialog" aria-modal="true">
+      <div class="fixed inset-0 flex">
+        <div
+          class="relative mr-8 flex w-full max-w-sm flex-1"
+          transition:drawer
+        >
+          {@render sidebar()}
+        </div>
+      </div>
+    </div>
+  {/if}
+
+  <!-- Static sidebar for desktop -->
+  <div
+    class="lg:border-bg-200 hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-80 lg:flex-col lg:border-r"
+  >
+    {@render sidebar()}
+  </div>
+
+  <div class="h-full lg:pl-80">
+    <div
+      class="sticky top-0 z-40 flex h-16 items-center justify-between gap-x-8 px-4 shadow-xs lg:px-8"
+    >
+      <button
+        type="button"
+        class="flex items-center rounded-full p-2 text-white transition-opacity duration-150 ease-in focus:outline-hidden lg:hidden"
+        class:opacity-0={navDrawerOpen}
+        class:opacity-100={!navDrawerOpen}
+        onclick={() => (navDrawerOpen = true)}
+      >
+        <span class="sr-only">Open sidebar</span>
+        <i class="iconify material-symbols--chevron-right size-6"></i>
+      </button>
+
+      {@render header()}
+    </div>
+
+    <main class="mx-auto -mt-16 h-full max-w-sm">
+      {@render main()}
+    </main>
+  </div>
+</div>
---
+++ apps/chat/src/routes/(app)/+layout.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Shell from "$lib/components/Shell.svelte";
+
+  // eslint-disable-next-line svelte/valid-prop-names-in-kit-pages
+  let { children } = $props();
+</script>
+
+<Shell>
+  {#snippet header()}
+    <button
+      type="button"
+      class="bg-blaze-400 ml-auto flex cursor-pointer items-center rounded-full px-8 py-2 text-black ring-4 ring-transparent hover:ring-black focus:ring-black focus:outline-hidden"
+      >Connect</button
+    >
+  {/snippet}
+
+  {#snippet nav()}
+    <ul class="flex flex-1 flex-col gap-y-7">
+      <li>
+        <div class="text-text-200 text-xs/6 font-semibold">Recent</div>
+        <ul class="-mx-2 mt-2 space-y-1">
+          <li>
+            <a
+              href="/view/:id"
+              class="group text-text-200 hover:text-blaze-300 focus:ring-blaze-300 focus:text-blaze-300 flex gap-x-3 rounded-lg p-2 text-sm/6 font-semibold focus:ring focus:outline-hidden"
+            >
+              <span
+                class="text-text-200 border-bg-200 flex size-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium group-hover:text-white"
+                >B</span
+              >
+              <span class="truncate">...</span>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="mt-auto flex justify-end py-4">
+        <button
+          type="button"
+          class="ring-bg-200 hover:text-blaze-700 hover:ring-blaze-800 focus:text-blaze-700 focus:ring-blaze-800 text-text-200 flex cursor-pointer items-center gap-x-2 rounded-full px-4 py-2 text-sm ring-2 focus:outline-hidden"
+        >
+          <i class="iconify material-symbols--square-rounded size-3"></i>
+          Stop All Chats
+        </button>
+      </li>
+    </ul>
+  {/snippet}
+
+  {#snippet main()}
+    {@render children()}
+  {/snippet}
+</Shell>
