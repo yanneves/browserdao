f1dab8a5498112aae1f84f7ed70e8173df25afa6
Yann イーベス Eves <hello@yanneves.com>
2025-05-21 21:38:29 +0100

    chore(apps/persona): Removed unused components to satisfy linter

--- apps/persona/src/routes/+page.svelte
+++ apps/persona/src/routes/+page.svelte
@@ -7,12 +7,9 @@
   import NavBar from "$lib/components/NavBar.svelte";
   import Presale from "$lib/components/Presale.svelte";
   import Resources from "$lib/components/Resources.svelte";
-  import Testimonial from "$lib/components/Testimonial.svelte";
   import Testimonials from "$lib/components/Testimonials.svelte";
   import Traits from "$lib/components/Traits.svelte";
   import Waitlist from "$lib/components/Waitlist.svelte";
-  import avatarImage1 from "$lib/images/avatars/avatar-1.png?enhanced&width=48&height=48";
-  import avatarImage2 from "$lib/images/avatars/avatar-2.png?enhanced&width=48&height=48";
 </script>
 
 <Hero />
