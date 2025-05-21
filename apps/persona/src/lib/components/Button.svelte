12cbcf81e4db39d4e945cd0494b114f784c9b2aa
cybrejon <cybrejon.senpai@gmail.com>
2025-05-21 22:25:58 +0800

    refactored button to have less moving parts for more maintainable code

--- apps/persona/src/lib/components/Button.svelte
+++ apps/persona/src/lib/components/Button.svelte
@@ -1,44 +1,17 @@
 <script>
-  let {
-    class: classes,
-    variant = "solid",
-    color = "zinc",
-    children,
-    ...rest
-  } = $props();
+  let { class: classes, variant = "solid", children, ...rest } = $props();
 
-  const baseStyles = {
+  const styles = {
     solid:
-      "inline-flex justify-center rounded-md py-1 px-4 text-base font-semibold tracking-tight shadow-sm focus:outline-hidden",
+      "inline-flex justify-center py-1 px-4 text-base font-medium tracking-tight shadow-sm focus:outline-hidden text-foreground hover:bg-foreground focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-foreground active:bg-foreground active:text-foreground/80 disabled:opacity-30 disabled:hover:text-foreground",
     outline:
-      "inline-flex justify-center rounded-md border py-[calc(--spacing(1)-1px)] px-[calc(--spacing(4)-1px)] text-base font-semibold tracking-tight focus:outline-hidden",
-  };
-
-  const variantStyles = {
-    solid: {
-      zinc: "bg-zinc-900 text-white hover:bg-zinc-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-900 active:bg-zinc-700 active:text-white/80 disabled:opacity-30 disabled:hover:bg-zinc-900",
-      blue: "bg-orange-600 text-white hover:bg-orange-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 active:bg-orange-700 active:text-white/80 disabled:opacity-30 disabled:hover:bg-orange-600",
-      white:
-        "bg-zinc-950 text-orange-600 hover:text-orange-700 focus-visible:text-orange-900 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white active:bg-orange-50 active:text-orange-900/80 disabled:opacity-40 disabled:hover:text-orange-600",
-    },
-    outline: {
-      zinc: "border-zinc-200 text-zinc-100 hover:border-zinc-300 hover:bg-zinc-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-600 active:border-zinc-200 active:bg-zinc-950 active:text-zinc-100/70 disabled:opacity-40 disabled:hover:border-zinc-200 disabled:hover:bg-transparent",
-      blue: "border-orange-300 text-orange-600 hover:border-orange-400 hover:bg-orange-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 active:text-orange-600/70 disabled:opacity-40 disabled:hover:border-orange-300 disabled:hover:bg-transparent",
-    },
+      "inline-flex justify-center border border-l-6 py-[calc(--spacing(1)-1px)] px-[calc(--spacing(4)-1px)] text-base font-medium tracking-tight focus:outline-hidden border-foreground text-foreground hover:border-foreground hover:bg-foreground/10 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-foreground active:text-foreground active:bg-foreground/20 active:text-foreground/70 disabled:opacity-40 disabled:hover:text-foreground disabled:hover:bg-transparent",
   };
 </script>
 
 <svelte:element
   this={typeof rest.href === "undefined" ? "button" : "a"}
-  class={[
-    baseStyles[variant],
-    variant === "outline"
-      ? variantStyles.outline[color]
-      : variant === "solid"
-        ? variantStyles.solid[color]
-        : undefined,
-    classes,
-  ]}
+  class={[styles[variant], classes]}
   {...rest}
 >
   {@render children?.()}</svelte:element
