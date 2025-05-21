6654d749124de15bec5ca5cb68ec0e19fdfb208c
cybrejon <cybrejon.senpai@gmail.com>
2025-05-21 22:25:08 +0800

    temporarily remove unnecessary sections

--- apps/persona/src/routes/+page.svelte
+++ apps/persona/src/routes/+page.svelte
@@ -19,33 +19,7 @@
 <Introduction />
 <NavBar />
 <Traits />
-<Testimonial
-  id="testimonial-from-tommy-stroman"
-  author={{
-    name: "Tommy Stroman",
-    role: "Front-end developer",
-    image: avatarImage1,
-  }}
->
-  <p>
-    “I didn’t know a thing about icon design until I read this book. Now I can
-    create any icon I need in no time. Great resource!”
-  </p>
-</Testimonial>
 <Comparison />
-<Testimonial
-  id="testimonial-from-gerardo-stark"
-  author={{
-    name: "Gerardo Stark",
-    role: "Creator of Pandemicons",
-    image: avatarImage2,
-  }}
->
-  <p>
-    “I’ve tried to create my own icons in the past but quickly got frustrated
-    and gave up. Now I sell my own custom icon sets online.”
-  </p>
-</Testimonial>
 <Resources />
 <Waitlist />
 <Presale />
