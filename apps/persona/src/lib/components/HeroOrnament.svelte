05eb07ad9791cb2f8de6a8b25435d9dd7440a6ce
cybrejon <cybrejon.senpai@gmail.com>
2025-05-23 17:10:13 +0800

    chore(apps/persona): added ornament below hero section to serve as a separator; removed unused code

---
+++ apps/persona/src/lib/components/HeroOrnament.svelte
@@ -0,0 +1,5 @@
+<div class="mx-auto grid w-[95%] grid-cols-12 gap-1 sm:gap-3 md:w-2/3 xl:w-1/2">
+  {#each Array(12).fill(0) as _}
+    <div class="bg-primary h-2 skew-x-12"></div>
+  {/each}
+</div>
--- apps/persona/src/routes/+page.svelte
+++ apps/persona/src/routes/+page.svelte
@@ -3,23 +3,21 @@
   import Creator from "$lib/components/Creator.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import Hero from "$lib/components/Hero.svelte";
+  import HeroOrnament from "$lib/components/HeroOrnament.svelte";
   import Introduction from "$lib/components/Introduction.svelte";
   import NavBar from "$lib/components/NavBar.svelte";
-  import Presale from "$lib/components/Presale.svelte";
   import Resources from "$lib/components/Resources.svelte";
-  import Testimonials from "$lib/components/Testimonials.svelte";
   import Traits from "$lib/components/Traits.svelte";
   import Waitlist from "$lib/components/Waitlist.svelte";
 </script>
 
 <Hero />
+<HeroOrnament />
 <Introduction />
 <NavBar />
 <Traits />
 <Comparison />
 <Resources />
 <Waitlist />
-<Presale />
-<Testimonials />
 <Creator />
 <Footer />
